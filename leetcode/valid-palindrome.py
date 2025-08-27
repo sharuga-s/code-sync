59e2b588cb921ccc4ab99d3aa038cafe399a3650
Sharuga Suthakaran <98789633+sharuga-s@users.noreply.github.com>
2025-08-27 14:35:29 -0400

    Update solution for valid-palindrome

--- leetcode/valid-palindrome.py
+++ leetcode/valid-palindrome.py
@@ -5,14 +5,29 @@ class Solution(object):
         :rtype: bool
         """
 
-        s = s.lower()
-        new = ""
-        for i in range(len(s)):
-            if s[i].isalpha() or s[i].isnumeric():
-                new += s[i]
+        # BEATS 5.03%
 
-        while len(new) > 1:
-            if new[0] != new[-1]:
+        # s = s.lower()
+        # new = ""
+        # for i in range(len(s)):
+        #     if s[i].isalpha() or s[i].isnumeric():
+        #         new += s[i]
+
+        # while len(new) > 1:
+        #     if new[0] != new[-1]:
+        #         return False
+        #     new = new[1:-1]
+        # return True
+
+        # BEATS 10.09%
+
+        news = ""
+        for i in s:
+            if i >= 'a' and i <= 'z' or i >= 'A' and i <= 'Z' or i >= '0' and i <= '9':
+                news += i
+        s = news
+        print(s)
+        for i in range(len(s)):
+            if s[i].lower() != s[len(s) - i - 1].lower():
                 return False
-            new = new[1:-1]
         return True
