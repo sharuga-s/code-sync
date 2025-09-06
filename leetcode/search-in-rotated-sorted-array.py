57dbb1554293389c8cc4fd4d0fea39be0e313659
Sharuga Suthakaran <98789633+sharuga-s@users.noreply.github.com>
2025-09-06 14:22:38 -0400

    Update solution for search-in-rotated-sorted-array

--- leetcode/search-in-rotated-sorted-array.py
+++ leetcode/search-in-rotated-sorted-array.py
@@ -6,42 +6,69 @@ class Solution(object):
         :rtype: int
         """
         
-        sort = nums
-        rot = 0
+        # sort = nums
+        # rot = 0
 
-        for i in range(0, len(nums) - 1):
-            if nums[i] > nums[i + 1]:
-                sort = nums[i + 1:] + nums[:i + 1]
-                rot = i + 1
-                break
+        # for i in range(0, len(nums) - 1):
+        #     if nums[i] > nums[i + 1]:
+        #         sort = nums[i + 1:] + nums[:i + 1]
+        #         rot = i + 1
+        #         break
         
-        print(sort)
-        print(rot)
+        # print(sort)
+        # print(rot)
 
-        idx = -1
+        # idx = -1
 
         low = 0
-        hi = len(sort) - 1
+        hi = len(nums) - 1
         mid = (low + hi) // 2
 
-        while low <= hi:
-            if sort[mid] == target:
-                idx = (mid + rot) % len(nums)
-                break
-            if sort[low] == target:
-                idx = (low + rot) % len(nums)
-                break
-            if sort[hi] == target:
-                idx = (hi + rot) % len(nums)
-                break
 
-            if target < sort[mid]:
-                hi = mid - 1
-                mid = (low + hi) // 2
-                continue
+        # while low <= hi:
+
+        #     if sort[mid] == target:
+        #         idx = (mid + rot) % len(nums)
+        #         break
+        #     if sort[low] == target:
+        #         idx = (low + rot) % len(nums)
+        #         break
+        #     if sort[hi] == target:
+        #         idx = (hi + rot) % len(nums)
+        #         break
+
+        #     if target < sort[mid]:
+        #         hi = mid - 1
+        #         mid = (low + hi) // 2
+        #         continue
             
-            else:
-                low = mid + 1
-                mid = (low + hi) // 2
+        #     else:
+        #         low = mid + 1
+        #         mid = (low + hi) // 2
 
-        return idx
+        # return idx
+            
+        low = 0
+        hi = len(nums) - 1
+
+        while low <= hi:
+            mid = (low + hi) // 2
+
+            if nums[mid] == target:
+                return mid
+
+        # check if the left side is sorted
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target and target < nums[mid]:
+                    hi = mid - 1
+                else:
+                    low = mid + 1
+
+            # if left side isn't sorted, the right side must be
+            else:
+                if nums[mid] < target and target <= nums[hi]:
+                    low = mid + 1
+                else:
+                    hi = mid - 1
+
+        return -1
