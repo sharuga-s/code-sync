c04b119e620557716d75347303b5ae04076e211e
Sharuga Suthakaran <98789633+sharuga-s@users.noreply.github.com>
2025-09-08 15:48:39 -0400

    Update solution for two-sum

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -6,7 +6,21 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        for i in range(0, len(nums)):
-            for j in range(i + 1, len(nums)):
-                if (nums[i] + nums[j]) == target:
-                    return [i, j]
+        # for i in range(0, len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if (nums[i] + nums[j]) == target:
+        #             return [i, j]
+
+        # return []
+
+        intmap = {}
+
+        for i in range(len(nums)):
+            complement = target - nums[i]
+
+            if complement in intmap:
+                return [i, intmap[complement]]
+            
+            intmap[nums[i]] = i
+
+        return []
