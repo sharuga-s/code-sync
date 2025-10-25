bcf5885044c18791db83b9c826061da8e1527420
Sharuga Suthakaran <98789633+sharuga-s@users.noreply.github.com>
2025-10-25 14:09:38 -0400

    Add solution for swap-nodes-in-pairs

---
+++ leetcode/swap-nodes-in-pairs.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+
+        new = ListNode(0)
+
+        new.next = head
+        prev = new
+
+        while head and head.next:
+            first = head
+            second = head.next
+
+            prev.next = second
+            first.next = second.next
+            second.next = first
+
+            prev = first
+            head = first.next
+
+        return new.next
