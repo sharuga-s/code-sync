4246fc44a8c3211c6eb9731091c5c5d7f6c13d53
Sharuga Suthakaran <98789633+sharuga-s@users.noreply.github.com>
2025-10-27 13:43:53 -0400

    Update solution for two-sum-ii-input-array-is-sorted

--- leetcode/two-sum-ii-input-array-is-sorted.py
+++ leetcode/two-sum-ii-input-array-is-sorted.py
@@ -1,10 +1,15 @@
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         
         start = 0
         end = len(numbers) - 1
 
-        while start <= end:
+        while start < end:
             sum = numbers[start] + numbers[end]
             if sum == target:
                 return [start + 1, end + 1]
