24c3cf6ef6bbd6f9195454d480c17f667e1c1530
Sharuga Suthakaran <98789633+sharuga-s@users.noreply.github.com>
2025-11-01 19:53:48 -0400

    Add solution for maximum-number-of-balloons

---
+++ leetcode/maximum-number-of-balloons.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxNumberOfBalloons(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+        
+        balmap = {"b":0, "a":0, "l":0, "o":0, "n":0}
+
+        for i in text:
+            if i in balmap:
+                balmap[i] += 1
+
+        count = 0
+        
+        x = min(balmap["b"], balmap["a"], balmap["n"])
+        if balmap["b"] >= x and balmap["a"] >= x and balmap["n"] >= x and balmap["l"] >= (2 * x) and balmap["o"] >= (2 * x):
+            return x
+        
+        return min(balmap["l"] // 2, balmap["o"] // 2)
