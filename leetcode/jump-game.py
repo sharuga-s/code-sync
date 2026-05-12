1a519c144f08fd3f03d968be8f0d01c56c8d933b
Sharuga Suthakaran <98789633+sharuga-s@users.noreply.github.com>
2026-05-12 19:28:17 -0400

    Update solution for jump-game

--- leetcode/jump-game.py
+++ leetcode/jump-game.py
@@ -4,7 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-
+        # max_len is the farthest index you can possibly reach given all the jumps you've made so far.
+        # greedy bc we always jump the max
         max_len = 0
         
         for i in range(len(nums)):
