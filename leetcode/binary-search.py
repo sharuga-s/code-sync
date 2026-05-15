403f028df7dc888d967defc26380ccb58d911578
Sharuga Suthakaran <98789633+sharuga-s@users.noreply.github.com>
2026-05-15 10:50:24 -0400

    Update solution for binary-search

--- leetcode/binary-search.py
+++ leetcode/binary-search.py
@@ -1,19 +1,22 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         
-        low = 0
-        hi = len(nums) - 1
-
-        while low <= hi:
-            mid = (low + hi) // 2
-
-            if target == nums[mid]:
+        l = 0
+        r = len(nums) - 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] == target:
                 return mid
 
             if target < nums[mid]:
-                hi = mid - 1
-            
+                r = mid - 1
             else:
-                low = mid + 1
+                l = mid + 1
 
         return -1
