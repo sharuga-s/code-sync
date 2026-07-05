0b3724bca71a1951ce0e3d17938ccd53d99b5c34
Sharuga Suthakaran <98789633+sharuga-s@users.noreply.github.com>
2026-07-04 21:33:22 -0400

    Add solution for best-time-to-buy-and-sell-stock-ii

---
+++ leetcode/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        
+        maxProfit = 0
+
+        for i in range(1, len(prices)):
+            if (prices[i] > prices[i - 1]):
+                maxProfit += prices[i] - prices[i - 1]
+
+        return maxProfit
